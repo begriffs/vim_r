a27773dd469e72775577ec47983b3fa09fe3bea6
Joe Nelson <cred+github@begriffs.com>
2013-03-20 10:12:47 -0700

    See more revealing test, do not cast to integer

--- spec/window_spec.rb
+++ spec/window_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe "VimR::Windows" do
   it "knows how many windows there are" do
-    vim_ruby_eval('VimR::Windows.count').to_i.should == 1
+    vim_ruby_eval('VimR::Windows.count').should == '1'
   end
 end
